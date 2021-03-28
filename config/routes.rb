771d2ce1d1b8add36a0316a36615629127ccee8c
yusuketakegawa <zyou06979@gmail.com>
2021-03-28 14:06:41 +0900

    ユーザーの詳細画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'books#index'
+  resources :users, only: :show
 
   resources :rooms, only: [:new, :create, :destroy] do
     resources :talks, only: [:index, :create]
---
+++ spec/factories/books.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :book do
+    title   {'本'}
+    author  {'著者'}
+    description   {'説明'}
+    genre_id   {2}
+    association :user
+  end
+end
